aa55ddbcf93cc6d9588f286787dfc94dfeae48b3
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-12 12:35:32 -0300

    refactor: move duplicated code to a helper

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,23 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	verifyCorrectMessage := func(t *testing.T, result, expect string) {
+		t.Helper()
+		if result != expect {
+			t.Errorf("result '%s', expect '%s'", result, expect)
+		}
+	}
+
 	t.Run("Sea hello to specified people", func(t *testing.T) {
 		name := "Junior"
 		result := Hello(name)
 		expect := "Hello, " + name
 
-		if result != expect {
-			t.Errorf("result '%s', expect '%s'", result, expect)
-		}
+		verifyCorrectMessage(t, result, expect)
 	})
 
 	t.Run("Sea hello to all world", func(t *testing.T) {
 		result := Hello("")
 		expect := "Hello, world"
 
-		if result != expect {
-			t.Errorf("result '%s', expect '%s'", result, expect)
-		}
+		verifyCorrectMessage(t, result, expect)
 	})
 
 }
