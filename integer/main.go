60620c7dd3dca08c082db728a4b4c4f26753db8a
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-12 12:52:17 -0300

    feat: Add example sum function

--- integer/main.go
+++ integer/main.go
@@ -1,8 +1,17 @@
 package integer
 
+import "fmt"
+
 // Sum is a sum of two numbers
 func Sum(numberOne, numberTwo int) int {
 	return numberOne + numberTwo
+}
+
+// ExampleSum is a example of Sum function
+func ExampleSum() {
+	sum := Sum(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
 
 func main() {
